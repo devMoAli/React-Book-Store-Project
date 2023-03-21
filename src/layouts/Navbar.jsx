934907102959470b6449c95f060e58b8cacdfc0a
devMoAli <dev.mohamed.ali1@gmail.com>
2023-03-21 09:12:28 +0000

    Redux-Toolkit-CRUD-App

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/" className="nav-link1">Home</Link>
-      <Link to="/show-books" className="nav-link2">Show Books</Link>
-      <Link to="/add-book" className="nav-link3">Add Book</Link>
-    </nav>
-  )
-}
+    <div className="navbar">
+      <div class="overlay"></div>
+      <div class="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className="nav-link1">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/show-books" className="nav-link2">
+                Show Books
+              </Link>
+            </li>
+            <li>
+              <Link to="/add-book" className="nav-link3">
+                Add Book
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
